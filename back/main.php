cbf43e801457092fe899f0733499f4074d29186c
Ethan <easonbest123@gmail.com>
2024-01-30 10:32:37 +0800

    後台恐龍完成

--- back/main.php
+++ back/main.php
@@ -1,2 +1,5 @@
-<h2>偶爾放鬆下，玩點恐龍吧</h2>
+<div class="container text-center">
+<h2 class="mb-1">偶爾放鬆下，玩點恐龍吧</h2>
  <?php include "./din/din.php"?> 
+ <!-- <iframe src="../din/din.html" frameborder="0"></iframe> -->
+ </div>
